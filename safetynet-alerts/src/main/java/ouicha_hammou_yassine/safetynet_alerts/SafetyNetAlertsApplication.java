9f2bb25265e7b78c13a0a782c044fd9f720dad7d
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-01-30 22:00:29 +0100

    First commit, SafetyNet Alerts app is created locally

---
+++ safetynet-alerts/src/main/java/ouicha_hammou_yassine/safetynet_alerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package ouicha_hammou_yassine.safetynet_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
---
+++ safetynet-alerts/src/test/java/ouicha_hammou_yassine/safetynet_alerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package ouicha_hammou_yassine.safetynet_alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
