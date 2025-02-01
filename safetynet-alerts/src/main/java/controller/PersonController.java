baf9c2181432764494af296b67715b8928c07c8d
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-01 20:10:13 +0100

    Adding the rest controller of the person class

---
+++ safetynet-alerts/src/main/java/controller/PersonController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.PersonService;
+
+import java.util.List;
+
+@RequestMapping("/person")
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Person> getAllPersons(){
+        return personService.getAllPersons();
+    }
+
+}
