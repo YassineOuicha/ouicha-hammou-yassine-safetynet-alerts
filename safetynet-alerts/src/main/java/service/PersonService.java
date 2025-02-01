e6813cf93734710810c0203069e8abaccad819be
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-01 19:52:33 +0100

    Adding the servicePerson

--- safetynet-alerts/src/main/java/service/PersonService.java
+++ safetynet-alerts/src/main/java/service/PersonService.java
@@ -1,6 +1,22 @@
 package service;
 
+import model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.DataRepository;
+
+import java.util.List;
+
+@Service
 public class PersonService {
+
+    @Autowired
+    private DataRepository dataRepository;
+
+    public List<Person> getAllPersons(){
+        return dataRepository.getPersons();
+    }
+
 
 }
 
