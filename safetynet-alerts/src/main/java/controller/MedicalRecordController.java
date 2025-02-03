2d579a0dd26cb081edafea46dad9c4ca7a71e1d9
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-03 11:31:36 +0100

    Adding the medical record controller

--- safetynet-alerts/src/main/java/controller/MedicalRecordController.java
+++ safetynet-alerts/src/main/java/controller/MedicalRecordController.java
@@ -1,11 +1,43 @@
 package controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import model.MedicalRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.MedicalRecordService;
+
+import java.util.List;
 
 @RequestMapping("medicalrecord")
 @RestController
 public class MedicalRecordController {
 
+    @Autowired
+    private MedicalRecordService medicalRecordService;
+
+    @GetMapping("/all")
+    public List<MedicalRecord> getAllMedicalRecords(){
+        return medicalRecordService.getAllMedicalRecords();
+    }
+
+    @GetMapping
+    public MedicalRecord getMedicalRecord(@RequestParam String firstName, @RequestParam String lastName){
+        return medicalRecordService.getMedicalRecord(firstName, lastName)
+                .orElseThrow(()-> new RuntimeException("Medical record not found"));
+    }
+
+    @PostMapping("/add")
+    public MedicalRecord addMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        return medicalRecordService.addMedicalRecord(medicalRecord);
+    }
+
+    @PutMapping("/update")
+    public boolean updateMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        return medicalRecordService.updateMedicalRecord(medicalRecord);
+    }
+
+    @DeleteMapping("/delete")
+    public boolean deleteMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        return medicalRecordService.deleteMedicalRecord(medicalRecord);
+    }
 
 }
