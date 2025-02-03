397bd9784783b8a3a10df43e96fb8a30f65e9b0f
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-03 10:19:39 +0100

    Updating FireStationService with CRUD Methodes

--- safetynet-alerts/src/main/java/service/DataService.java
+++ safetynet-alerts/src/main/java/service/DataService.java
@@ -29,4 +29,5 @@ public class DataService {
         return dataRepository;
     }
 
+
 }
--- safetynet-alerts/src/main/java/service/FireStationService.java
+++ safetynet-alerts/src/main/java/service/FireStationService.java
@@ -6,17 +6,46 @@ import org.springframework.stereotype.Service;
 import repository.DataRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FireStationService {
 
     @Autowired
-    private DataService dataService;
+    private final DataService dataService;
+
+    public FireStationService(DataService dataService) {
+        this.dataService = dataService;
+    }
 
     public List<FireStation> getAllFireStations(){
         DataRepository dataRepository = dataService.getData();
-
         return dataRepository.getFireStations();
+    }
+
+    public Optional<FireStation> getFireStation(int station){
+        return getAllFireStations().stream().filter(f -> (f.getStation() == station)).findFirst();
+    }
+
+    public FireStation addFireStation(FireStation fireStation){
+        Optional<FireStation> existingFireStation = getFireStation(fireStation.getStation());
+        if(existingFireStation.isPresent()){
+            throw new RuntimeException("This FireStation exists already");
+        }
+        dataService.getData().getFireStations().add(fireStation);
+        return fireStation;
+    }
+
+    public void upDateFireStation(FireStation updatedFireStation){
+
+        FireStation oldFireStation = getFireStation(updatedFireStation.getStation())
+                .orElseThrow(()-> new RuntimeException("Fire Station doesn't exists"));
+        oldFireStation.setStation(updatedFireStation.getStation());
+        oldFireStation.setAddress(updatedFireStation.getAddress());
+    }
+
+    public boolean deleteFireStation(FireStation fireStation){
+        return getAllFireStations().removeIf(f -> f.getStation() == fireStation.getStation());
     }
 
 }
