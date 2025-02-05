c9f6a61feec1f6514fe375184b6fdb95763ddc60
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-05 14:50:51 +0100

    Commenting the code of the controllers

--- safetynet-alerts/src/main/java/com/safetynet/controller/MedicalRecordController.java
+++ safetynet-alerts/src/main/java/com/safetynet/controller/MedicalRecordController.java
@@ -7,34 +7,69 @@ import com.safetynet.service.MedicalRecordService;
 
 import java.util.List;
 
-
+/**
+ * Controller for handling requests related to medical records.
+ * Provides endpoints to retrieve, add, update, and delete medical record information.
+ */
 @RestController
 public class MedicalRecordController {
 
     @Autowired
     private MedicalRecordService medicalRecordService;
 
+    /**
+     * Endpoint to retrieve a list of all medical records.
+     *
+     * @return a list of all medical records
+     */
     @GetMapping("/medicalrecords")
     public List<MedicalRecord> getAllMedicalRecords(){
         return medicalRecordService.getAllMedicalRecords();
     }
 
+    /**
+     * Endpoint to retrieve a medical record based on a combination of the first and the last name.
+     *
+     * @param firstName the first name of the person whose medical record is to be retrieved
+     * @param lastName the last name of the person whose medical record is to be retrieved
+     * @return the medical record of the person
+     * @throws RuntimeException if the medical record is not found for the given name combination
+     */
     @GetMapping("/medicalrecord")
     public MedicalRecord getMedicalRecord(@RequestParam String firstName, @RequestParam String lastName){
         return medicalRecordService.getMedicalRecord(firstName, lastName)
                 .orElseThrow(()-> new RuntimeException("Medical record not found"));
     }
 
+    /**
+     * Endpoint to add a new medical record.
+     *
+     * @param medicalRecord the medical record information to be added
+     * @return the added medical record information
+     */
     @PostMapping("/medicalrecord")
     public MedicalRecord addMedicalRecord(@RequestBody MedicalRecord medicalRecord){
         return medicalRecordService.addMedicalRecord(medicalRecord);
     }
 
+    /**
+     * Endpoint to update an existing medical record.
+     *
+     * @param medicalRecord the updated medical record information
+     * @return true if the medical record was successfully updated, false otherwise
+     */
     @PutMapping("/medicalrecord")
     public boolean updateMedicalRecord(@RequestBody MedicalRecord medicalRecord){
         return medicalRecordService.updateMedicalRecord(medicalRecord);
     }
 
+    /**
+     * Endpoint to delete a medical record based on a combination of the first and the last name.
+     *
+     * @param firstName the first name of the person whose medical record is to be deleted
+     * @param lastName the last name of the person whose medical record is to be deleted
+     * @return true if the medical record was successfully deleted, false otherwise
+     */
     @DeleteMapping("/medicalrecord")
     public boolean deleteMedicalRecord(@RequestParam String firstName, @RequestParam String lastName){
         MedicalRecord medicalRecord = getMedicalRecord(firstName, lastName);
--- safetynet-alerts/src/main/java/com/safetynet/controller/PersonController.java
+++ safetynet-alerts/src/main/java/com/safetynet/controller/PersonController.java
@@ -15,41 +15,92 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    /**
+     * Endpoint to retrieve a list of all persons.
+     *
+     * @return a list of all persons
+     */
     @GetMapping("/persons")
     public List<Person> getAllPersons(){
         return personService.getAllPersons();
     }
 
+    /**
+     * Endpoint to retrieve a person based on a combination of the first name and the last name.
+     *
+     * @param firstName the first name of the person to retrieve
+     * @param lastName the last name of the person to retrieve
+     * @return the person's détails, corresponding to the provided first and last name
+     * @throws RuntimeException if the person is not found
+     */
     @GetMapping ("/person")
     public Person getPerson(@RequestParam String firstName, @RequestParam String lastName){
         return personService.getPerson(firstName, lastName)
                 .orElseThrow(()-> new RuntimeException("Person not found"));
     }
+
+    /**
+     * Endpoint to retrieve a list of children living at a specific address.
+     *
+     * @param address the address to check for children
+     * @return a list containing children information for the given address
+     */
     @GetMapping("/childAlert")
     public List<ChildAlertDTO> getChildrenByAddress(@RequestParam("address") String address){
         return personService.getChildrenByAddress(address);
     }
 
+    /**
+     * Endpoint to retrieve persons information by last name.
+     *
+     * @param lastName the last name of the persons to retrieve information for
+     * @return a list containing person details for each person who has the same last name
+     */
     @GetMapping("/personInfo")
     public List<PersonInfoDTO> getPersonInfoByLastName(@RequestParam String lastName){
         return personService.getPersonInfoByLastName(lastName);
     }
 
+    /**
+     * Endpoint to retrieve a list of community email addresses for a given city.
+     *
+     * @param city the city to get the community email addresses for
+     * @return a list of email addresses for the specified city
+     */
     @GetMapping("/communityEmail")
     public List<String> getCommunityEmails(@RequestParam String city){
         return personService.getEmailsByCity(city);
     }
 
+    /**
+     * Endpoint to add a new person.
+     *
+     * @param person the person data to be added
+     * @return the added person information
+     */
     @PostMapping("/person")
     public Person addPerson(@RequestBody Person person){
         return personService.addPerson(person);
     }
 
+    /**
+     * Endpoint to update an existing person.
+     *
+     * @param person the updated person data
+     * @return true if the person was successfully updated, false otherwise
+     */
     @PutMapping("/person")
     public boolean updatePerson(@RequestBody Person person){
         return personService.updatePerson(person);
     }
 
+    /**
+     * Endpoint to delete a person based on a combination of the first name and the last name.
+     *
+     * @param firstName the first name of the person to delete
+     * @param lastName the last name of the person to delete
+     * @return true if the person was successfully deleted, false otherwise
+     */
     @DeleteMapping("/person")
     public boolean deletePerson(@RequestParam String firstName, @RequestParam String lastName){
         return personService.deletePerson(firstName, lastName);
